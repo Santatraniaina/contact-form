a8bb0688e006ddfc30c6e7e2f045415a40be3559
Santatraniaina <santatraniaina21@gmail.com>
2025-05-15 23:09:17 +0300

    Fix Form Submission and Error handling

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function toggleRadio(event) {
     console.log(target);
 }
 
-function displayError(input) {
+function displayError(parent, input) {
     const errorMessage = input.dataset.error;
     const errorElement = document.createElement('span');
     errorElement.classList.add('form-error-message');
     errorElement.textContent = errorMessage;
-    input.parentNode.appendChild(errorElement);
+    parent.appendChild(errorElement);
     return 1;
 }
 
@@ -36,13 +36,27 @@ function submitForm(event) {
     let errorCount = 0;
 
     inputGroups.forEach(inputGroup => {
-        const input = inputGroup.querySelector('.form-input');
-        if (input && (input.value === '' || input.value === undefined)) {
-            errorCount += displayError(input);
+        const input = inputGroup.querySelector('input:not([type="radio"])');
+        const textarea = inputGroup.querySelector('textarea');
+        const radios = inputGroup.querySelectorAll('input[type="radio"]');
+
+        // Input validation
+        if (input && input.required && (input.value === '' || input.value === undefined || (input.type === "checkbox" && !input.checked))) {
+            errorCount += displayError(inputGroup, input);
+        }
+        // Textarea validation
+        if (textarea && textarea.required && (textarea.value === '' || textarea.value === undefined)) {
+            errorCount += displayError(inputGroup, textarea);
+        }
+        // Radio validation
+        if (radios.length > 0) {
+            const checkedRadio = Array.from(radios).some(radio => radio.checked);
+            if (!checkedRadio) {
+                errorCount += displayError(inputGroup, inputGroup.querySelector(".form-input-radio"));
+            }
         }
     });
 
-    console.log('Form submitted');
     if (errorCount > 0) {
         console.log(`Form has ${errorCount} errors`);
     } else {
@@ -50,7 +64,9 @@ function submitForm(event) {
         setTimeout(() => {
             success.classList.remove('active');
         }, 3000);
+        console.log('Form submitted');
     }
+
     console.log(data);
 }
 
